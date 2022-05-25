7dc5db7d91312d82f15475bcec2590c0131dfdd7
novifitri <animangap@gmail.com>
2022-05-25 19:06:50 +0700

    init

---
+++ tugas27.js
@@ -0,0 +1,12 @@
+//gabung string
+let str1 = "No"
+let str2 = "rumah saya"
+let str3 = "30"
+let str4 = "A"
+let gabung = str1.concat(" ", str2, " ",str3 ," ", str4)
+console.log(gabung)
+
+let str5 = "Jalan pangeran Tirtayasa No"
+let angka = 70
+let gabung2 = str5.concat(" ", angka)
+console.log(gabung2)
